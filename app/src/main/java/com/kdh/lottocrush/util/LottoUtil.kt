56f1f8c069e86019d38aee0429a71034b8c975e2
dodo <ehgus4688@gmail.com>
2022-06-16 16:24:36 +0900

    ignore 업뎃

--- app/src/main/java/com/kdh/lottocrush/util/LottoUtil.kt
+++ app/src/main/java/com/kdh/lottocrush/util/LottoUtil.kt
@@ -7,12 +7,12 @@ import java.util.*
 object LottoUtil {
 
     fun getNextEpisodeBasedonDate(): Int {
-        val startDate = "2002-12-14 23:59:59"
+        val startDate = "2002-12-07 23:59:59"
         val dateFormat = SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
         val cDate = Date()
         val sDate: Date = dateFormat.parse(startDate) as Date
         val diff: Long = cDate.time - sDate.time
-        val nextEpi = diff / (86400 * 1000 * 7) + 2
+        val nextEpi = diff / (86400 * 1000 * 7) + 1
         Log.d("dodo55 ","cDate ${cDate}")
         Log.d("dodo55 ","sDate ${sDate}")
         Log.d("dodo55 ","diff ${diff}")
